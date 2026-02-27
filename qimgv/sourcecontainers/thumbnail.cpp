d878d1b6e0c882627a05d85d54ad5f116d7fb6dc
yeezylife <30175425+yeezylife@users.noreply.github.com>
2026-02-27 21:57:08 +0800

    Update thumbnail.cpp

--- qimgv/sourcecontainers/thumbnail.cpp
+++ qimgv/sourcecontainers/thumbnail.cpp
@@ -1,31 +1,32 @@
 #include "thumbnail.h"
 
-Thumbnail::Thumbnail(QString _name, QString _info, int _size, std::shared_ptr<QPixmap> _pixmap)
+// 优化：使用成员初始化列表，并在初始化列表中处理逻辑，确保所有成员都被初始化
+Thumbnail::Thumbnail(const QString &_name, const QString &_info, int _size, std::shared_ptr<QPixmap> _pixmap)
     : mName(_name),
       mInfo(_info),
       mPixmap(_pixmap),
-      mSize(_size)
+      mSize(_size),
+      // 修复：如果 _pixmap 为空，mHasAlphaChannel 必须初始化为 false，否则原代码会导致该变量值不确定
+      mHasAlphaChannel(_pixmap ? _pixmap->hasAlphaChannel() : false)
 {
-    if(_pixmap)
-        mHasAlphaChannel = _pixmap->hasAlphaChannel();
 }
 
-QString Thumbnail::name() {
+QString Thumbnail::name() const {
     return mName;
 }
 
-QString Thumbnail::info() {
+QString Thumbnail::info() const {
     return mInfo;
 }
 
-int Thumbnail::size() {
+int Thumbnail::size() const {
     return mSize;
 }
 
-bool Thumbnail::hasAlphaChannel() {
+bool Thumbnail::hasAlphaChannel() const {
     return mHasAlphaChannel;
 }
 
-std::shared_ptr<QPixmap> Thumbnail::pixmap() {
+std::shared_ptr<QPixmap> Thumbnail::pixmap() const {
     return mPixmap;
 }

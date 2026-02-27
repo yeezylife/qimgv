f077ae70f91f5b3d6676c18cb611aa1067984f66
yeezylife <30175425+yeezylife@users.noreply.github.com>
2026-02-27 21:56:45 +0800

    Update thumbnail.h

--- qimgv/sourcecontainers/thumbnail.h
+++ qimgv/sourcecontainers/thumbnail.h
@@ -6,14 +6,19 @@
 
 class Thumbnail {
 public:
-    Thumbnail(QString _name, QString _info, int _size, std::shared_ptr<QPixmap> _pixmap);
-    QString name();
-    QString info();
-    int size();
-    bool hasAlphaChannel();
-    std::shared_ptr<QPixmap> pixmap();
+    // 优化：QString 使用 const 引用传递，避免不必要的拷贝
+    Thumbnail(const QString &_name, const QString &_info, int _size, std::shared_ptr<QPixmap> _pixmap);
+    
+    // 优化：添加 const 修饰符，符合常量正确性规范
+    QString name() const;
+    QString info() const;
+    int size() const;
+    bool hasAlphaChannel() const;
+    std::shared_ptr<QPixmap> pixmap() const;
+
 private:
-    QString mName, mInfo;
+    QString mName;
+    QString mInfo;
     std::shared_ptr<QPixmap> mPixmap;
     int mSize;
     bool mHasAlphaChannel;

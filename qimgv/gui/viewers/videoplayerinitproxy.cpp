b4c556bec0312145539232a8eb3be7075b83e1d1
yeezylife <30175425+yeezylife@users.noreply.github.com>
2026-03-05 16:17:31 +0800

    modified:   qimgv/gui/viewers/videoplayerinitproxy.cpp 	modified:   qimgv/gui/viewers/videoplayerinitproxy.h

--- qimgv/gui/viewers/videoplayerinitproxy.cpp
+++ qimgv/gui/viewers/videoplayerinitproxy.cpp
@@ -1,43 +1,35 @@
 #include "videoplayerinitproxy.h"
-#include <QApplication>
-#include <QDir>
 
 #ifdef _QIMGV_PLAYER_PLUGIN
-    #define QIMGV_PLAYER_PLUGIN_STR _QIMGV_PLAYER_PLUGIN
+    #define QIMGV_PLAYER_PLUGIN _QIMGV_PLAYER_PLUGIN
 #else
-    #define QIMGV_PLAYER_PLUGIN_STR ""
+    #define QIMGV_PLAYER_PLUGIN ""
 #endif
 
 VideoPlayerInitProxy::VideoPlayerInitProxy(QWidget *parent)
     : VideoPlayer(parent),
-      player(nullptr),
-      layout(this)
+      player(nullptr)
 {
     setAccessibleName("VideoPlayerInitProxy");
     setMouseTracking(true);
-    layout.setContentsMargins(0, 0, 0, 0);
+    layout.setContentsMargins(0,0,0,0);
     setLayout(&layout);
-
     connect(settings, &Settings::settingsChanged, this, &VideoPlayerInitProxy::onSettingsChanged);
-    
-    libFile = QIMGV_PLAYER_PLUGIN_STR;
-
+    libFile = QIMGV_PLAYER_PLUGIN;
 #ifdef _WIN32
     libDirs << QApplication::applicationDirPath() + "/plugins";
 #else
     QDir libPath(QApplication::applicationDirPath() + "/../lib/qimgv");
-    libDirs << (libPath.exists() ? libPath.absolutePath() : ".") 
-            << "/usr/lib/qimgv" 
-            << "/usr/lib64/qimgv";
+    libDirs << (libPath.makeAbsolute() ? libPath.path() : ".") << "/usr/lib/qimgv" << "/usr/lib64/qimgv";
 #endif
 }
 
 VideoPlayerInitProxy::~VideoPlayerInitProxy() {
-    // shared_ptr 会自动处理 player 的析构
 }
 
 void VideoPlayerInitProxy::onSettingsChanged() {
-    if (!player) return;
+    if(!player)
+        return;
     player->setMuted(!settings->playVideoSounds());
     player->setVideoUnscaled(!settings->expandImage());
 }
@@ -47,44 +39,41 @@ std::shared_ptr<VideoPlayer> VideoPlayerInitProxy::getPlayer() {
 }
 
 bool VideoPlayerInitProxy::isInitialized() {
-    return isPlayerInitialized();
+    return (player != nullptr);
 }
 
-bool VideoPlayerInitProxy::initPlayer() {
+inline bool VideoPlayerInitProxy::initPlayer() {
 #ifndef USE_MPV
     return false;
 #endif
-    if (player) return true;
+    if(player)
+        return true;
 
     QFileInfo pluginFile;
-    bool found = false;
-    for (const QString& dir : libDirs) {
+    for(auto dir : libDirs) {
         pluginFile.setFile(dir + "/" + libFile);
-        if (pluginFile.isFile() && pluginFile.isReadable()) {
+        if(pluginFile.isFile() && pluginFile.isReadable()) {
             playerLib.setFileName(pluginFile.absoluteFilePath());
-            found = true;
             break;
         }
     }
-
-    if (!found) {
-        qDebug() << "Could not find" << libFile << "in:" << libDirs;
+    if(playerLib.fileName().isEmpty()) {
+        qDebug() << "Could not find" << libFile << "in the following directories:" << libDirs;
         return false;
     }
 
+// load lib
     typedef VideoPlayer* (*createPlayerWidgetFn)();
-    createPlayerWidgetFn fn = (createPlayerWidgetFn)playerLib.resolve("CreatePlayerWidget");
-    if (fn) {
+    createPlayerWidgetFn fn = (createPlayerWidgetFn) playerLib.resolve("CreatePlayerWidget");
+    if(fn) {
         VideoPlayer* pl = fn();
         player.reset(pl);
     }
-
-    if (!player) {
-        qDebug() << "Could not load:" << playerLib.fileName() << ". Plugin error?";
+    if(!player) {
+        qDebug() << "Could not load:" << playerLib.fileName() << ". Wrong plugin version?";
         return false;
     }
 
-    // 初始化配置
     player->setMuted(!settings->playVideoSounds());
     player->setVideoUnscaled(!settings->expandImage());
     player->setVolume(settings->volume());
@@ -93,107 +82,127 @@ bool VideoPlayerInitProxy::initPlayer() {
     layout.addWidget(player.get());
     player->hide();
     setFocusProxy(player.get());
-
-    // 信号转发
     connect(player.get(), SIGNAL(durationChanged(int)), this, SIGNAL(durationChanged(int)));
     connect(player.get(), SIGNAL(positionChanged(int)), this, SIGNAL(positionChanged(int)));
     connect(player.get(), SIGNAL(videoPaused(bool)),    this, SIGNAL(videoPaused(bool)));
     connect(player.get(), SIGNAL(playbackFinished()),   this, SIGNAL(playbackFinished()));
 
-    if (eventFilterObj)
-        player->installEventFilter(eventFilterObj);
+    if(eventFilterObj)
+        player.get()->installEventFilter(eventFilterObj);
 
     return true;
 }
 
 bool VideoPlayerInitProxy::showVideo(QString file) {
-    if (!initPlayer()) return false;
+    if(!initPlayer())
+        return false;
     return player->showVideo(file);
 }
 
 void VideoPlayerInitProxy::seek(int pos) {
-    if (isPlayerInitialized()) player->seek(pos);
+    if(!player)
+        return;
+    player->seek(pos);
 }
 
 void VideoPlayerInitProxy::seekRelative(int pos) {
-    if (isPlayerInitialized()) player->seekRelative(pos);
+    if(!player)
+        return;
+    player->seekRelative(pos);
 }
 
 void VideoPlayerInitProxy::pauseResume() {
-    if (isPlayerInitialized()) player->pauseResume();
+    if(!player)
+        return;
+    player->pauseResume();
 }
 
 void VideoPlayerInitProxy::frameStep() {
-    if (isPlayerInitialized()) player->frameStep();
+    if(!player)
+        return;
+    player->frameStep();
 }
 
 void VideoPlayerInitProxy::frameStepBack() {
-    if (isPlayerInitialized()) player->frameStepBack();
+    if(!player)
+        return;
+    player->frameStepBack();
 }
 
 void VideoPlayerInitProxy::stop() {
-    if (isPlayerInitialized()) player->stop();
+    if(!player)
+        return;
+    player->stop();
 }
 
 void VideoPlayerInitProxy::setPaused(bool mode) {
-    if (isPlayerInitialized()) player->setPaused(mode);
+    if(!player)
+        return;
+    player->setPaused(mode);
 }
 
 void VideoPlayerInitProxy::setMuted(bool mode) {
-    if (isPlayerInitialized()) player->setMuted(mode);
+    if(!player)
+        return;
+    player->setMuted(mode);
 }
 
 bool VideoPlayerInitProxy::muted() {
-    return isPlayerInitialized() ? player->muted() : true;
+    if(!player)
+        return true;
+    return player->muted();
 }
 
 void VideoPlayerInitProxy::volumeUp() {
-    if (isPlayerInitialized()) {
-        player->volumeUp();
-        settings->setVolume(player->volume());
-    }
+    if(!player)
+        return;
+    player->volumeUp();
+    settings->setVolume(player->volume());
 }
 
 void VideoPlayerInitProxy::volumeDown() {
-    if (isPlayerInitialized()) {
-        player->volumeDown();
-        settings->setVolume(player->volume());
-    }
+    if(!player)
+        return;
+    player->volumeDown();
+    settings->setVolume(player->volume());
 }
 
 void VideoPlayerInitProxy::setVolume(int vol) {
-    if (isPlayerInitialized()) player->setVolume(vol);
+    if(!player)
+        return;
+    player->setVolume(vol);
 }
 
 int VideoPlayerInitProxy::volume() {
-    return isPlayerInitialized() ? player->volume() : 0;
+    if(!player)
+        return 0;
+    return player->volume();
 }
 
 void VideoPlayerInitProxy::setVideoUnscaled(bool mode) {
-    if (isPlayerInitialized()) player->setVideoUnscaled(mode);
+    if(!player)
+        return;
+    player->setVideoUnscaled(mode);
 }
 
 void VideoPlayerInitProxy::setLoopPlayback(bool mode) {
-    if (isPlayerInitialized()) player->setLoopPlayback(mode);
+    if(!player)
+        return;
+    player->setLoopPlayback(mode);
 }
 
 void VideoPlayerInitProxy::show() {
-    if (initPlayer()) {
-        if (errorLabel) {
-            layout.removeWidget(errorLabel);
-            errorLabel->deleteLater();
-            errorLabel = nullptr;
-        }
+    if(initPlayer()) {
+        layout.removeWidget(errorLabel);
         player->show();
-    } else if (!errorLabel) {
+    } else if(!errorLabel) {
         errorLabel = new QLabel(this);
         errorLabel->setAttribute(Qt::WA_TransparentForMouseEvents);
         errorLabel->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
-        
-        QString errString = QString("Could not load %1 from:").arg(libFile);
-        for (const auto& path : libDirs) {
-            errString += "\n" + path + "/";
-        }
+        //errorLabel->setAlignment(Qt::AlignVCenter);
+        QString errString = "Could not load " + libFile + " from:";
+        for(auto path : libDirs)
+            errString.append("\n" + path + "/");
         errorLabel->setText(errString);
         layout.addWidget(errorLabel);
     }
@@ -201,7 +210,8 @@ void VideoPlayerInitProxy::show() {
 }
 
 void VideoPlayerInitProxy::hide() {
-    if (player) player->hide();
+    if(player)
+        player->hide();
     VideoPlayer::hide();
 }
 
@@ -211,10 +221,11 @@ void VideoPlayerInitProxy::paintEvent(QPaintEvent *event) {
 
 void VideoPlayerInitProxy::installEventFilter(QObject *filterObj) {
     eventFilterObj = filterObj;
-    if (player) player->installEventFilter(eventFilterObj);
+    if(player)
+        player->installEventFilter(eventFilterObj);
 }
 
 void VideoPlayerInitProxy::removeEventFilter(QObject *filterObj) {
-    eventFilterObj = nullptr;
-    if (player) player->removeEventFilter(filterObj);
+    if(player)
+        player->removeEventFilter(filterObj);
 }
--- qimgv/gui/viewers/videoplayerinitproxy.h
+++ qimgv/gui/viewers/videoplayerinitproxy.h
@@ -4,50 +4,46 @@
 
 #include <memory>
 #include <QVBoxLayout>
+#include "videoplayer.h"
+#include "settings.h"
 #include <QPainter>
 #include <QLibrary>
 #include <QLabel>
 #include <QFileInfo>
 #include <QDebug>
-#include "videoplayer.h"
-#include "settings.h"
 
 class VideoPlayerInitProxy : public VideoPlayer {
-    Q_OBJECT
-    Q_DISABLE_COPY(VideoPlayerInitProxy)
 public:
     VideoPlayerInitProxy(QWidget *parent = nullptr);
     ~VideoPlayerInitProxy();
-
-    bool showVideo(QString file) override;
-    void seek(int pos) override;
-    void seekRelative(int pos) override;
-    void pauseResume() override;
-    void frameStep() override;
-    void frameStepBack() override;
-    void stop() override;
-    void setPaused(bool mode) override;
-    void setMuted(bool mode) override;
-    bool muted() override;
-    void volumeUp() override;
-    void volumeDown() override;
-    void setVolume(int vol) override;
-    int volume() override;
-    void setVideoUnscaled(bool mode) override;
-    void setLoopPlayback(bool mode) override;
-    
+    bool showVideo(QString file);
+    void seek(int pos);
+    void seekRelative(int pos);
+    void pauseResume();
+    void frameStep();
+    void frameStepBack();
+    void stop();
+    void setPaused(bool mode);
+    void setMuted(bool);
+    bool muted();
+    void volumeUp();
+    void volumeDown();
+    void setVolume(int);
+    int volume();
+    void setVideoUnscaled(bool mode);
+    void setLoopPlayback(bool mode);
     std::shared_ptr<VideoPlayer> getPlayer();
     bool isInitialized();
 
-    void installEventFilter(QObject *filterObj) override;
-    void removeEventFilter(QObject *filterObj) override;
+    void installEventFilter(QObject *filterObj);
+    void removeEventFilter(QObject *filterObj);
 
 public slots:
     void show();
     void hide();
 
 protected:
-    void paintEvent(QPaintEvent *event) override;
+    void paintEvent(QPaintEvent *event);
 
 private:
     QLibrary playerLib;
@@ -60,15 +56,10 @@ private:
     QString libFile;
     QStringList libDirs;
 
-    // 内联辅助函数
-    inline bool isPlayerInitialized() const { return player != nullptr; }
-    inline void checkPlayerInitialized(const char* funcName) const {
-        if (!player) qDebug() << "Player not initialized, skipping" << funcName;
-    }
-
 private slots:
     void onSettingsChanged();
 
 signals:
     void playbackFinished();
+
 };

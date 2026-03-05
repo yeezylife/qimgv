56e8375aae3542f151578c7980844bf9ed00f31c
yeezylife <30175425+yeezylife@users.noreply.github.com>
2026-03-05 21:12:22 +0800

    编译修复

--- qimgv/components/directorymanager/watchers/watcherevent.h
+++ qimgv/components/directorymanager/watchers/watcherevent.h
@@ -11,21 +11,21 @@ public:
         Modify
     };
 
-    WatcherEvent(const QString &name,int timerId, Type type = None);
-    WatcherEvent(const QString& name, uint cookie, int timerId, Type type = None);
-    ~WatcherEvent();
+    WatcherEvent(const QString &name,int timerId, Type type = None) noexcept;
+    WatcherEvent(const QString& name, uint cookie, int timerId, Type type = None) noexcept;
+    ~WatcherEvent() noexcept;
 
-    QString name() const;
-    void setName(const QString& name);
+    QString name() const noexcept;
+    void setName(const QString& name) noexcept;
 
-    uint cookie() const;
-    void setCookie(uint cookie);
+    uint cookie() const noexcept;
+    void setCookie(uint cookie) noexcept;
 
-    int timerId() const;
-    void setTimerId(int timerId);
+    int timerId() const noexcept;
+    void setTimerId(int timerId) noexcept;
 
-    Type type() const;
-    void setType(Type type);
+    Type type() const noexcept;
+    void setType(Type type) noexcept;
 
 private:
     QString mName;

570129915aead6930ace795b33cc8c9ffe1390dc
yeezylife <30175425+yeezylife@users.noreply.github.com>
2026-03-24 13:55:37 +0800

    编译修复

--- qimgv/gui/customwidgets/sidepanelwidget.cpp
+++ qimgv/gui/customwidgets/sidepanelwidget.cpp
@@ -4,7 +4,6 @@ SidePanelWidget::SidePanelWidget(QWidget *parent) : QWidget(parent) {
 
 }
 
-// 添加实现，调用基类 QWidget 的 show 方法
 void SidePanelWidget::show() {
     QWidget::show();
 }
--- qimgv/gui/customwidgets/sidepanelwidget.h
+++ qimgv/gui/customwidgets/sidepanelwidget.h
@@ -7,11 +7,8 @@ class SidePanelWidget : public QWidget
     Q_OBJECT
 public:
     explicit SidePanelWidget(QWidget *parent = nullptr);
-
-signals:
+    virtual ~SidePanelWidget() = default;
 
 public slots:
-    // 移除 = 0，使其不再是纯虚函数，避免链接错误
-    // 保留 virtual 允许派生类重写
     virtual void show();
 };

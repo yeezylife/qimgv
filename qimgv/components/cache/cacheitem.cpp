76336d85ae0d5c5db75a26bf6f1896057b196237
yeezylife <30175425+yeezylife@users.noreply.github.com>
2026-04-12 14:22:24 +0800

    实现cacheitem原子化读写函数

--- qimgv/components/cache/cacheitem.cpp
+++ qimgv/components/cache/cacheitem.cpp
@@ -1,10 +1,16 @@
 #include "cacheitem.h"
 
-CacheItem::CacheItem(std::shared_ptr<Image> contents)
+CacheItem::CacheItem(std::shared_ptr<Image> contents) noexcept
     : mContents(std::move(contents))
 {
 }
 
-std::shared_ptr<Image> CacheItem::getContents() const {
-    return mContents;
+std::shared_ptr<Image> CacheItem::getContents() const noexcept {
+    // acquire 保证读取到完整对象
+    return std::atomic_load_explicit(&mContents, std::memory_order_acquire);
+}
+
+void CacheItem::setContents(std::shared_ptr<Image> contents) noexcept {
+    // release 保证写入对其他线程可见
+    std::atomic_store_explicit(&mContents, std::move(contents), std::memory_order_release);
 }
--- qimgv/components/cache/cacheitem.h
+++ qimgv/components/cache/cacheitem.h
@@ -6,10 +6,12 @@ class Image;
 
 class CacheItem {
 public:
-    explicit CacheItem(std::shared_ptr<Image> contents);
+    explicit CacheItem(std::shared_ptr<Image> contents) noexcept;
 
-    std::shared_ptr<Image> getContents() const;
+    std::shared_ptr<Image> getContents() const noexcept;
+    void setContents(std::shared_ptr<Image> contents) noexcept;
 
 private:
-    std::shared_ptr<Image> mContents;
+    // ⚠️ 必须 mutable（因为 atomic_load 需要非 const 指针）
+    mutable std::shared_ptr<Image> mContents;
 };
